13209a6833ccdde20914f3aef47e4f02a95e9b94
rcc706 <rcc706@outlook.com>
2025-05-01 19:48:59 -0500

    updated

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -4,3 +4,34 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String portfolioName;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+    public Long getportfolioId() {
+        return portfolioId;
+    }
+
+    public String getportfolioName() {
+        return portfolioName;
+    }
+
+    public void setportfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+}
--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -4,3 +4,82 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String securityName;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private String purchasePrice;
+
+    @Column(nullable = false)
+    private String quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(String securityName, String category, String purchaseDate, float purchasePrice, int quantity) {
+        this.securityName = securityName;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+    }
+
+    public Long getsecurityId() {
+        return securityId;
+    }
+
+    public String getsecurityName() {
+        return securityName;
+    }
+
+    public void setsecurityName(String securityName) {
+        this.securityName = securityName;
+    }
+
+    public String getcategory() {
+        return category;
+    }
+
+    public void setcategory(String category) {
+        this.category = category;
+    }
+
+    public String getpurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setpurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public float getpurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setpurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public int getquantity() {
+        return quantity;
+    }
+
+    public void setquantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
